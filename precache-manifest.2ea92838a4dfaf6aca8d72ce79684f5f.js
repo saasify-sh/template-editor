a9e4aea079fb3e0a2df8f530496d4c9b0277b121
Travis Fischer <fisch0920@gmail.com>
2020-03-17 14:26:09 -0400

    Updates

---
+++ precache-manifest.2ea92838a4dfaf6aca8d72ce79684f5f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d688f8c4e435c00dc1285801deea919",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f27504c769902c5fffac",
+    "url": "/static/css/2.f7347798.chunk.css"
+  },
+  {
+    "revision": "d91b7b3b10f7ee476a70",
+    "url": "/static/css/main.111c8263.chunk.css"
+  },
+  {
+    "revision": "f27504c769902c5fffac",
+    "url": "/static/js/2.da101cd1.chunk.js"
+  },
+  {
+    "revision": "cb6539cd98edb7a4525a11443c4a9b49",
+    "url": "/static/js/2.da101cd1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d91b7b3b10f7ee476a70",
+    "url": "/static/js/main.bbfe0db7.chunk.js"
+  },
+  {
+    "revision": "0b5400085f9f4805e424",
+    "url": "/static/js/runtime-main.2a14cc81.js"
+  },
+  {
+    "revision": "e148643a1c2cea004bd9d9377b8c7542",
+    "url": "/static/media/logo.e148643a.svg"
+  }
+]);
